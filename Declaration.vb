4e75a8ac5915ff91dbeebfb94829cda417cc4ec8
Azrinatiqah@365office.digital <Azrinatiqah@365office.digital>
2022-06-03 23:40:58 +0800

    Add project files.

---
+++ Declaration.vb
@@ -0,0 +1,8 @@
+﻿Module Declaration
+    Public ds As New DataSet
+    Public cmd As New OleDb.OleDbCommand
+    Public da As New OleDb.OleDbDataAdapter
+    Public con As OleDb.OleDbConnection = New OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Acer\Desktop\labtask4.db.mdb")
+    Public gender As String
+    Public race As String
+End Module
